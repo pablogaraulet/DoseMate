82b0d67d69105989af53be3d1e55cbf9456b8c9a
100475293 <100475293@alumnos.uc3m.es>
2025-04-21 19:43:14 -0700

    web page

--- server.py
+++ server.py
@@ -54,13 +54,12 @@ def dashboard():
 def home():
     return render_template("index.html")
 
-# --- About page ---
-@app.route("/about")
-def about():
-    return render_template("about.html")
+# --- Contact page ---
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
 
 # --- Start the Flask server ---
 if __name__ == '__main__':
     init_db()
     app.run(host='0.0.0.0', port=5000)
-
